d15d331312cc1a5f960f91d2b7603518e1dddce5
shashanklokanadham <shashanklokanadham43@gmail.com>
2018-05-01 23:44:12 +0530

    first commit

--- sample.php
+++ sample.php
@@ -1,52 +1,78 @@
 <!DOCTYPE html>
+
 <html>
+
 <body>
 
+
 <h1>Placing a text in custom image</h1>
+
 
 <?php
 
   //select the image on which you would like to write the text
-  $source = "wallpaper.jpg";
-  
-  //create the image from existing image
-  $image = imagecreatefromjpeg($source);
+ 
+ $sourcelocation = $_POST["imgfile"];
 
-  //destination file name where you want to store the new image
+  
+  
+//create the image from existing image
+  
+$image = imagecreatefromjpeg($sourcelocation);
+
+  
+//destination file name where you want to store the new image
+
   $output = "images/textimage1.jpg";
 
  
-  //declaring the colors
-  $white = imagecolorallocate($image,255,255,255);
-  $black = imagecolorallocate($image,0,0,0);
+ 
+ //declaring the colors
+  
+$white = imagecolorallocate($image,255,255,255);
+  
+$black = imagecolorallocate($image,0,0,0);
 
-  //font size of the text
+ 
+ //font size of the text
+
   $font_size = 40;
 
 
-  //rotation of the text
-  $rotation = 0;
+ 
+ //rotation of the text
+  
+$rotation = 0;
 
-  //x- coordinate where you want to pace the text
-  $origin_x = 100;
+  
+//x- coordinate where you want to pace the text
 
-
-  //y- coordinate where you want to pace the text
-  $origin_y = 200;
-
-
-  //font style of the text.we need to give the location of.ttf file
-  $font = "font\open-sans\OpenSans-Bold.ttf";
-
-  //text you want to add to the image
-  $text="Hello! this is Shashank";
+  $origin_x = $_POST["xcoordinate"];
 
 
-  $text1 = imagettftext($image,$font_size,$rotation,$origin_x,$origin_y,$white,$font,$text);
+  
+//y- coordinate where you want to pace the text
 
-  imagejpeg($image,$output);
+  $origin_y =$_POST["ycoordinate"];
+
+
+  
+//font style of the text.we need to give the location of.ttf file
+ 
+ $font = "font\open-sans\OpenSans-Bold.ttf";
+
+ 
+ //text you want to add to the image
+ 
+ $text=$_POST["textinp"];
+$text1 = imagettftext($image,$font_size,$rotation,$origin_x,$origin_y,$white,$font,$text);
+
+ 
+ imagejpeg($image,$output);
+
 
 ?>
+
 <img src = "<?php echo $output?>">
 
 </body>
